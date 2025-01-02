5094d0699d3d5b684f3172b1846cf894062a714f
daniyal077 <131246344+daniyal077@users.noreply.github.com>
2025-01-02 22:38:57 +0500

    updated

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-import pickle
+import joblib
 from flask import Flask, jsonify, request, render_template
-import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
-model = pickle.load(open('regmodel.pkl', 'rb'))
-scaler = pickle.load(open('scaler.pkl', 'rb'))
+# Load the trained pipeline
+pipeline = joblib.load('trained_pipeline.pkl')
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the data from the form
-    features = [float(request.form[x]) for x in request.form]
+    try:
+        data = request.form.to_dict(flat=True)  
+        
+        input_data = pd.DataFrame([data]) 
+        
+        input_data = input_data.astype(float)
+        
+        prediction = pipeline.predict(input_data)
+        
+        return render_template('home.html', prediction_text=f"The house price prediction is {prediction[0]:.2f}")
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
-    # Transform the input data using the scaler
-    final_input = scaler.transform(np.array(features).reshape(1, -1))
-
-    # Make prediction using the model
-    prediction = model.predict(final_input)[0]
-
-    # Render the home.html template with prediction result
-    return render_template('home.html', prediction_text=f"The house price prediction is {prediction}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
